9e27a47ada703890d09b084bbc4940a79262086a
Prasanga Shrestha <saugatgiri15@gmail.com>
2022-06-29 16:14:26 +1000

    fetch requests made to a json file/job data displayed

--- src/views/jobs/JobDetails.vue
+++ src/views/jobs/JobDetails.vue
@@ -1,20 +1,29 @@
 <template>
-  <h1>Job Details Page</h1>
-  <p>The job id is: {{id}}</p>
-  <p>The title of job: {{title}}</p>
+<div v-if="jobs">
+  <h1>{{jobs.title}}</h1>
+  <p>The job id is: {{ id }}</p>
+  <p>details: {{jobs.details}}</p>
+  </div>
+  <div v-else>Loading Job Details:</div>
+
 </template>
 
 <script>
 export default {
-    props: ['id', 'title'],
-    // data() {
-    //     return {
-    //         id: this.$route.params.id
-    //     }
-    // }
-}
+  props: ["id"],
+
+  data() {
+    return {
+      jobs: null,
+    };
+  },
+  mounted() {
+    fetch("http://localhost:3000/jobs/" + this.id)
+      .then(res => res.json())
+      .then(data => (this.jobs = data))
+      .catch(err => console.log(err.message));
+  },
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
--- src/views/jobs/JobsView.vue
+++ src/views/jobs/JobsView.vue
@@ -8,15 +8,21 @@
 </template>
 
 <script>
+
 export default {
     data() {
         return {
-            jobs: [
-                {title:'Ninja UX Designer', id: 1, details: 'lorem'},
-                {title:'Ninja UX Developer', id: 2, details: 'lorem'},
-                {title:'Ninja Vue Developer', id: 3, details: 'lorem'}
-            ]
+            jobs: []
         }
+    },
+    mounted() {
+        fetch('http://localhost:3000/jobs')
+            .then(res => res.json())
+            .then(data => this.jobs = data)
+            .catch((err) => {
+                console.log('Error has occured');
+                console.log(err.message);
+            } )
     }
 
 }
