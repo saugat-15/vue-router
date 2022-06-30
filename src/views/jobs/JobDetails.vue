3597aad567cf815c1aedabb920b1f51a3cfc258c
Prasanga Shrestha <saugatgiri15@gmail.com>
2022-06-30 16:39:29 +1000

    jobdetails updated json

--- src/views/jobs/JobDetails.vue
+++ src/views/jobs/JobDetails.vue
@@ -1,11 +1,15 @@
 <template>
-<div v-if="jobs">
-  <h1>{{jobs.title}}</h1>
-  <p>The job id is: {{ id }}</p>
-  <p>details: {{jobs.details}}</p>
+  <div v-if="jobs">
+    <h1>{{ jobs.title }}</h1>
+    <p>The job id is: {{ id }}</p>
+    <p>details: {{ jobs.details }}</p>
+    <!-- <p>Count: {{ count }}</p> -->
   </div>
   <div v-else>Loading Job Details:</div>
 
+  <!-- <button @click="increaseCount">increase count</button>
+  <button @click="decreaseCount">decrease count</button>
+  <button @click="resetCount">reset count</button> -->
 </template>
 
 <script>
@@ -15,13 +19,25 @@ export default {
   data() {
     return {
       jobs: null,
+    //   count: 0,
     };
   },
+//   methods: {
+//     increaseCount() {
+//       this.count++;
+//     },
+//     decreaseCount() {
+//       this.count--;
+//     },
+//     resetCount() {
+//       this.count = 0;
+//     },
+//   },
   mounted() {
     fetch("http://localhost:3000/jobs/" + this.id)
-      .then(res => res.json())
-      .then(data => (this.jobs = data))
-      .catch(err => console.log(err.message));
+      .then((res) => res.json())
+      .then((data) => (this.jobs = data))
+      .catch((err) => console.log(err.message));
   },
 };
 </script>
